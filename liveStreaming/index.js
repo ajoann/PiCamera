c405a505e58e4e7b70cf5cbebd117f3097e06270
Amanda <ajhwhiz@gmail.com>
2017-11-26 16:22:27 -0500

    changed w & h of stream ; created stream folder and temp .txt file

--- liveStreaming/index.js
+++ liveStreaming/index.js
@@ -74,7 +74,7 @@ function startStreaming(io) {
     return;
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+  var args = ["-w", "400", "-h", "300", "-o", "./stream/image_stream.jpg", "-t", "10000", "-tl", "100"];
   proc = spawn('raspistill', args);
 
   console.log('Watching for changes...');
